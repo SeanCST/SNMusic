80866d6794726f77d29f0e6f31ca142a04bc5926
SeanCST <seancst.x@gmail.com>
2018-11-16 13:59:26 +0800

    Initial Commit

---
+++ SNMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SNMusic
+//
+//  Created by 肖恩伟 on 2018/11/16.
+//  Copyright © 2018 肖恩伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SNMusic/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SNMusic
+//
+//  Created by 肖恩伟 on 2018/11/16.
+//  Copyright © 2018 肖恩伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
